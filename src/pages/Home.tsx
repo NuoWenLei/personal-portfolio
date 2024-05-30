2f22e1550cd753cab3b714b0be87fbd52ab67f6c
NuoWenLei <nuowen0612@gmail.com>
2024-05-30 08:00:27 +0800

    edit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -115,7 +115,7 @@ export default function Home() {
       newURI = newURI + "&project=" + encodeURIComponent(visibleProject);
     }
 
-    window.history.replaceState(null, `Nuo Wen Lei - ${visiblePage}`, newURI);
+    window.history.replaceState(null, "", newURI);
   }, [visiblePage, visibleProject]);
 
   useEffect(() => {
